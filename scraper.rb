16a613dc3cbfd754053280af750c72c1f5c6e6fb
Matthew Landauer <mlandauer@gmail.com>
2019-06-20 11:14:32 +1000

    Update to latest version of the gem

--- scraper.rb
+++ scraper.rb
@@ -1,10 +1,3 @@
 require "masterview_scraper"
 
-MasterviewScraper.scrape_and_save_period(
-  url: "https://datracker.wsc.nsw.gov.au/Modules/applicationmaster",
-  period: :thisweek,
-  params: {
-    "4a" => "WLUA,82AReview,CDC,DA,Mods",
-    "6" => "F"
-  }
-)
+MasterviewScraper.scrape_and_save(:wingecarribee)
