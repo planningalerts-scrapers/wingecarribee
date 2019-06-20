da83834f7007afc120aa8953b93ebb06bee3c0e2
Matthew Landauer <mlandauer@gmail.com>
2019-06-20 11:09:55 +1000

    Update ruby scraper

--- scraper.rb
+++ scraper.rb
@@ -1,7 +1,10 @@
 require "masterview_scraper"
 
-url = "https://datracker.wsc.nsw.gov.au/Modules/applicationmaster/default.aspx?page=found&1=thisweek&4a=WLUA,82AReview,CDC,DA,Mods&6=F"
-
-MasterviewScraper.scrape(url) do |record|
-  MasterviewScraper.save(record)
-end
+MasterviewScraper.scrape_and_save_period(
+  url: "https://datracker.wsc.nsw.gov.au/Modules/applicationmaster",
+  period: :thisweek,
+  params: {
+    "4a" => "WLUA,82AReview,CDC,DA,Mods",
+    "6" => "F"
+  }
+)
